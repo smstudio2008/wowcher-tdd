9c34d8ef6a10ba35aaae2e1fd0d42e32de230d49
ss mo <mansatech2018@gmail.com>
2019-09-01 12:53:55 +0100

    adding js folder struture

--- js/index.js
+++ js/index.js
@@ -1,34 +1,40 @@
-const express = require('express')
-const app = express()
-const service = require('./service')
+const express = require('express');
+const service = require('./app/services/service');
+const app = express();
 
 var userCount = 0;
 
-app.get('/', (req, res) => {res.send('Status: OK')})
+app.get('/', (req, res) => {
+	res.send('Status: OK');
+});
 
 app.get('/users', (req, res) => {
-  const results = require('./resources/users.json')
-  userCount += results.length
-  const payload = {results, 'userCount': userCount}
-  res.send(payload)
-})
+	const results = require('./resources/users.json');
+	userCount += results.length;
+	const payload = { results, userCount: userCount };
+
+	res.send(payload);
+});
 
 app.get('/orders/user/:name', (req, res) => {
-  const userName = req.params.name
-  const orderAmount = service.getOrderCountForUser(userName);
-  res.send({'ordersByCustomer': orderAmount});
-})
+	const userName = req.params.name;
+	const orderAmount = service.getOrderCountForUser(userName);
+
+	res.send({ ordersByCustomer: orderAmount });
+});
 
 app.get('/orders/product/:product', (req, res) => {
-  const productName = req.params.product
-  const orderAmount = service.getOrderCountForProduct(productName);
-  res.send({'numberOfOrders': orderAmount});
-})
+	const productName = req.params.product;
+	const orderAmount = service.getOrderCountForProduct(productName);
+
+	res.send({ numberOfOrders: orderAmount });
+});
 
 app.get('/users/product/:product', (req, res) => {
-  const productName = req.params.product
-  const customers = service.getCustomerNamesForProduct({'customerOrdered': productName});
-  res.send(customers);
-})
+	const productName = req.params.product;
+	const customers = service.getCustomerNamesForProduct({ customerOrdered: productName });
 
-app.listen(3007, () => console.log('Listening on port 3007'))
+	res.send(customers);
+});
+
+app.listen(3007, () => console.log('Listening on port 3007'));
